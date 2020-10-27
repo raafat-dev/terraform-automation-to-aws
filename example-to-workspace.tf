d86ca81d5c1360bc96be8275f176ecc17caa64c8
mohamed raafat <raafat@localhost.localdomain>
2020-10-27 08:32:41 +0000

    workspace example

---
+++ example-to-workspace.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+
+  access_key = "access-key"
+  secret_key = "secret-key"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = lookup(var.instance_type,terraform.workspace)
+}
+
+variable "instance_type" {
+  type = "map"
+
+  default = {
+    default = "t2.nano"
+    dev     = "t2.micro"
+    prd     = "t2.large"
+  }
+}
